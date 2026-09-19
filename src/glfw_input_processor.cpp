2670847ce6624acc7cc219fb9bcd358ba3af21ed
Simon O'Neill <oneill.simon92@gmail.com>
2026-09-19 22:18:38 +0100

    Fix is_key_pressed edge semantics and scroll directions

--- src/glfw_input_processor.cpp
+++ src/glfw_input_processor.cpp
@@ -1,7 +1,5 @@
-//
-// Created by simon on 06/12/2020.
-//
-
+//// Created by simon on 06/12/2020.//
+#include <algorithm>
 #include "glfw_input_processor.h"
 
 void key_callback(void *window, int key, int scancode, int action, int mods) {
@@ -49,9 +47,15 @@ bool GlfwInputProcessor::is_key_down(int key) {
 }
 
 bool GlfwInputProcessor::is_key_pressed(int key) {
-    auto result = this->key_states[key] == GLFW_PRESS;
-    this->key_states[key] = NO_STATE;
-    return result;
+    // The first poll of a press records it for the rest of the frame so that
+    // every caller sees the edge exactly once per frame, and never across
+    // frames.
+    if (this->key_states[key] == GLFW_PRESS) {
+        this->key_states[key] = GLFW_REPEAT;
+        this->pressed_this_frame.push_back(key);
+        return true;
+    }
+    return std::find(this->pressed_this_frame.begin(), this->pressed_this_frame.end(), key) != this->pressed_this_frame.end();
 }
 
 bool GlfwInputProcessor::is_key_released(int key) {
@@ -63,26 +67,29 @@ bool GlfwInputProcessor::is_key_repeating(int key) {
 }
 
 bool GlfwInputProcessor::is_scrolling(const ScrollDirection& direction) {
-    if (this->scroll_x == 1) {
-        this->scroll_x = 0;
-        return direction == ScrollDirection::LEFT || direction == ScrollDirection::ANY;
-    }
-
-    if (this->scroll_x == -1) {
+    // GLFW reports positive horizontal offsets for rightward scrolls and
+    // positive vertical offsets for upward scrolls.
+    if (this->scroll_x > 0) {
         this->scroll_x = 0;
         return direction == ScrollDirection::RIGHT || direction == ScrollDirection::ANY;
     }
-
-    if (this->scroll_y == 1) {
+    if (this->scroll_x < 0) {
+        this->scroll_x = 0;
+        return direction == ScrollDirection::LEFT || direction == ScrollDirection::ANY;
+    }
+    if (this->scroll_y > 0) {
         this->scroll_y = 0;
         return direction == ScrollDirection::UP || direction == ScrollDirection::ANY;
     }
-
-    if (this->scroll_y == -1) {
+    if (this->scroll_y < 0) {
         this->scroll_y = 0;
         return direction == ScrollDirection::DOWN || direction == ScrollDirection::ANY;
     }
     return false;
+}
+
+void GlfwInputProcessor::begin_frame() {
+    this->pressed_this_frame.clear();
 }
 
 int GlfwInputProcessor::get_input_mode(int mode) const {
